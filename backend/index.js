639f1c744a99b1025d5809631c62a19a1ee33579
Afdal-B <159702730+Afdal-B@users.noreply.github.com>
2024-05-31 00:51:06 +0200

    update

--- backend/index.js
+++ backend/index.js
@@ -6,19 +6,22 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  try {
-    const r = await axios.put(
-      "https://api.chatengine.io/users/",
-      { username: username, secret: username, first_name: username },
-      { headers: { "private-key": "3566b4bf-4f7d-4f74-8fa7-8a58e95b18cd" } }
-    );
-    return res.status(r.status).json(r.data);
-  } catch (e) {
-    return res.status(e.response.status).json(e.response.data);
+app.post(
+  "https://chat-app-backend-rouge.vercel.app/authenticate",
+  async (req, res) => {
+    const { username } = req.body;
+    try {
+      const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        { username: username, secret: username, first_name: username },
+        { headers: { "private-key": "3566b4bf-4f7d-4f74-8fa7-8a58e95b18cd" } }
+      );
+      return res.status(r.status).json(r.data);
+    } catch (e) {
+      return res.status(e.response.status).json(e.response.data);
+    }
+    return res.json({ username: username, secret: "sha256..." });
   }
-  return res.json({ username: username, secret: "sha256..." });
-});
+);
 
 app.listen(3001);
