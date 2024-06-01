113437e7ce2ddc7cffa784c26a59cbe1c2c2342f
Afdal-B <159702730+Afdal-B@users.noreply.github.com>
2024-06-01 15:53:22 +0200

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import AuthPage from "./components/AuthPage";
 import ChatsPage from "./components/ChatsPage";
 function App() {
-  const [user, setUser] = useState(undefined);
+  return <h1>TTTEEEESSSSSTTT</h1>;
+
+  /*const [user, setUser] = useState(undefined);
 
   if (!user) {
     return <AuthPage onAuth={(user) => setUser(user)} />;
   } else {
     return <ChatsPage user={user}></ChatsPage>;
-  }
+  }*/
 }
 
 export default App;
